4ff08294f8c3231aa5bc8bfbeb19c7e6b69b8e7d
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-07-18 18:34:46 -0600

    Added/edited workout.js, index.js

---
+++ Develop/models/workout.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter the exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter the name of the exercise"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter the exercise duration in minutes"
+                },
+                weight: {
+                    type: Number,
+                    required: "Enter your weight in pounds"
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
