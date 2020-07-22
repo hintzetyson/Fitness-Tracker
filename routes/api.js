33e81c1da544c38d1547a44f3186665a5dec312a
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-07-22 14:28:50 -0600

    Finished api.js routes

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,41 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
-    Workout.find({}).limit(10)
+    Workout.find()
     .then((data) => res.json(data))
     .catch((err) => res.json(err));
 });
 
-router.put
-router.post
-router.get
-router.delete
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
+
+router.put("/api/workout/:id", ({ body, params }, res) =>{
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body} },
+
+        { new: true, runValidators: true }
+    )
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+})
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+})
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+});
+
+module.exports = router
